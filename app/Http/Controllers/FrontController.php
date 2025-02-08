088f92e2d00e9c19c5a943a70ff76cfca7e1fd06
pasipasipada <irfanoputra27@gmail.com>
2025-02-08 14:43:48 +0700

    feat: add front controller

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Handphone;
+use App\Services\FrontService;
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    protected $frontServices;
+
+    public function __construct(FrontService $frontServices) // DIP depedency injection
+    {
+        $this->frontServices = $frontServices;
+    }
+
+    public function index()
+    {
+        $data = $this->frontServices->getFrontPageData();
+        // dd($data);
+        return view('front.index', $data);
+    }
+
+    public function details(Handphone $handphone) // model binding
+    {
+        return view('front.details', compact('handphone'));
+    }
+
+    public function category(Category $category)
+    {
+        return view('front.category', compact('category'));
+    }
+}
