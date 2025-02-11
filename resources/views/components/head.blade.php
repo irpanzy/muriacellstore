0faa103dfc1b8de7f0c92c7160c24e7d9dd286fc
pasipasipada <irfanoputra27@gmail.com>
2025-02-11 20:07:01 +0700

    feat: add components head

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('output.css') }}" rel="stylesheet">
+    <title>Muria Cell</title>
+    <link rel="icon" type="{{asset('assets/images/logos/logo.png')}}" href="{{asset('assets/images/logos/logo.png')}}">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+</head>
